cc4f4d9a5047632e9451983f30b74351e53094e3
Mark Hine <mhine@juliusspencer.co.nz>
2024-11-15 17:25:22 +1100

    Implemented the searchQuery flow

--- data/src/main/java/com/sonder/data/repositories/SearchRepositoryImpl.kt
+++ data/src/main/java/com/sonder/data/repositories/SearchRepositoryImpl.kt
@@ -1,12 +1,16 @@
 package com.sonder.data.repositories
 
+import com.sonder.data.models.SearchRequestParams
+import com.sonder.data.models.SearchSectionResult
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 class SearchRepositoryImpl @Inject constructor() : SearchRepository {
 
-    override val searchQuery: Flow<String>
-        get() = TODO("Not yet implemented")
+    private val _searchQuery = MutableStateFlow("")
+    override val searchQuery: Flow<String> = _searchQuery.asStateFlow()
 
     override fun clearSectionSearchResults() {
         TODO("Not yet implemented")
