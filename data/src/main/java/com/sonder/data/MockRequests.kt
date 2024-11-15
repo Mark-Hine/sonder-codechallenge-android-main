221c9da953360ae2b9ec81da2b54984215f0feb1
Mark Hine <mhine@juliusspencer.co.nz>
2024-11-15 17:29:43 +1100

    Unwrapped the MockRequests from flows

--- data/src/main/java/com/sonder/data/MockRequests.kt
+++ data/src/main/java/com/sonder/data/MockRequests.kt
@@ -2,80 +2,58 @@ package com.sonder.data
 
 import com.sonder.data.models.SearchContentType
 import com.sonder.data.models.SearchRequestParams
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 object MockRequests {
 
-    fun getErrorRequestParams(): Flow<SearchRequestParams> = flow {
-        val request = SearchRequestParams(
-            sectionTitle = "Resources",
-            query = "error",
-            size = 10,
-            contentTypes = listOf(
-                SearchContentType.ARTICLE.value,
-                SearchContentType.CALL_PATHWAY.value
-            )
-        )
+	val errorRequestParams = SearchRequestParams(
+		sectionTitle = "Resources",
+		query = "error",
+		size = 10,
+		contentTypes = listOf(
+			SearchContentType.ARTICLE.value,
+			SearchContentType.CALL_PATHWAY.value
+		)
+	)
 
-        emit(request)
-    }
+	val horizontalCompactRequestParams = SearchRequestParams(
+		sectionTitle = "Topics",
+		query = "mock",
+		size = 3,
+		contentTypes = listOf(SearchContentType.CATEGORY_HUB.value)
+	)
 
-    fun getHorizontalCompactRequestParams(): Flow<SearchRequestParams> = flow {
-        val request = SearchRequestParams(
-            sectionTitle = "Topics",
-            query = "mock",
-            size = 3,
-            contentTypes = listOf(SearchContentType.CATEGORY_HUB.value)
-        )
+	val verticalCompactRequestParams = SearchRequestParams(
+		sectionTitle = "Categories",
+		query = "mock",
+		size = 4,
+		contentTypes = listOf(
+			SearchContentType.ARTICLE.value,
+			SearchContentType.CATEGORY_HUB.value,
+			SearchContentType.ACTION.value
+		)
+	)
 
-        emit(request)
-    }
+	val horizontalDetailedRequestParams = SearchRequestParams(
+		sectionTitle = "Tools and services",
+		query = "mock",
+		size = 6,
+		contentTypes = listOf(
+			SearchContentType.ASSESSMENT.value,
+			SearchContentType.ACTION.value,
+			SearchContentType.CALL_PATHWAY.value,
+			SearchContentType.SERIES.value,
+			SearchContentType.ARTICLE.value
+		)
+	)
 
-    fun getVerticalCompactRequestParams(): Flow<SearchRequestParams> = flow {
-        val request = SearchRequestParams(
-            sectionTitle = "Categories",
-            query = "mock",
-            size = 4,
-            contentTypes = listOf(
-                SearchContentType.ARTICLE.value,
-                SearchContentType.CATEGORY_HUB.value,
-                SearchContentType.ACTION.value
-            )
-        )
-
-        emit(request)
-    }
-
-    fun getHorizontaDetailedRequestParams(): Flow<SearchRequestParams> = flow {
-        val request = SearchRequestParams(
-            sectionTitle = "Tools and services",
-            query = "mock",
-            size = 6,
-            contentTypes = listOf(
-                SearchContentType.ASSESSMENT.value,
-                SearchContentType.ACTION.value,
-                SearchContentType.CALL_PATHWAY.value,
-                SearchContentType.SERIES.value,
-                SearchContentType.ARTICLE.value
-            )
-        )
-
-        emit(request)
-    }
-
-    fun getVerticalDetailedRequestParams(): Flow<SearchRequestParams> = flow {
-        val request = SearchRequestParams(
-            sectionTitle = "Resources",
-            query = "mock",
-            size = 10,
-            contentTypes = listOf(
-                SearchContentType.ARTICLE.value,
-                SearchContentType.CALL_PATHWAY.value
-            )
-        )
-
-        emit(request)
-    }
+	val verticalDetailedRequestParams = SearchRequestParams(
+		sectionTitle = "Resources",
+		query = "mock",
+		size = 10,
+		contentTypes = listOf(
+			SearchContentType.ARTICLE.value,
+			SearchContentType.CALL_PATHWAY.value
+		)
+	)
 
 }
